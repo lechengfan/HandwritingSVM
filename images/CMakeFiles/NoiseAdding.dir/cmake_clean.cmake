931b04c043bd1b8376e666e411d4524223744c5e
Quan Nguyen <qnnguyen@stanford.edu>
2014-11-08 15:31:57 -0800

    noiseadding for n copies

---
+++ images/CMakeFiles/NoiseAdding.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NoiseAdding.dir/noiseadding.cc.o"
+  "NoiseAdding.pdb"
+  "NoiseAdding"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/NoiseAdding.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- images/noiseadding.cc
+++ images/noiseadding.cc
@@ -1,3 +1,4 @@
+#include <opencv2/opencv.hpp>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <iostream>
@@ -7,29 +8,35 @@ using namespace std;
 
 int main(int argc, char** argv) {
 
-  if( argc != 2)
+  if( argc != 3)
     {
-     cout <<" Usage: display_image ImageToLoadAndDisplay" << endl;
+     cout <<" Usage: display_image ImageToLoadAndDisplay n" << endl;
      return -1;
     }
 
-    Mat imGray;
+    Mat imGray, result;
     imGray = imread(argv[1], CV_LOAD_IMAGE_GRAYSCALE);   // Read the file
     
-    if(! image.data )                              // Check for invalid input
+    if(! imGray.data )                              // Check for invalid input
     {
         cout <<  "Could not open or find the image" << std::endl ;
         return -1;
     }
-    Mat noise = Mat(imGray.size(),CV_64F);
-    normalize(imGray, result, 0.0, 1.0, CV_MINMAX, CV_64F);
-    randn(noise, 0, 0.05);
-    result = result + noise;
-    normalize(result, result, 0.0, 1.0, CV_MINMAX, CV_64F);
-    namedWindow( "Display window", WINDOW_AUTOSIZE );// Create a window for display.
-    imshow("OUTPUT",result);
+	int n = strtol(argv[2], NULL, 0);
+	string name = argv[1];
+	for (int i = 0; i < n; i++) {
+		Mat noise = Mat(imGray.size(),CV_64F);
+		normalize(imGray, result, 0.0, 1.0, CV_MINMAX, CV_64F);
+		randn(noise, 0, 0.05);
+		result = result + noise;
+		normalize(result, result, 0.0, 1.0, CV_MINMAX, CV_64F);
+		namedWindow( "Display window", WINDOW_AUTOSIZE );// Create a window for display.
+		//imshow("OUTPUT",result);
 
-    waitKey(0);                                          // Wait for a keystroke
-    //   in the window
+		//waitKey(0);                                          // Wait for a keystroke
+		//   in the window
+		result.convertTo(result, CV_8UC3, 255.0);
+		imwrite(to_string(i) + name, result);
+	}
     return 0;
 }
