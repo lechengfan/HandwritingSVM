55a3f0f55bb735b2d7fc37ff33948d2d46c2d2a1
root <qnnguyen@stanford.edu>
2014-12-06 00:36:10 -0500

    Remove unnecessary files and directories

--- README.md
+++
@@ -1,3 +0,0 @@
-Handwriting Recognition with Support Vector Machines
-====================================================
-Quan Nguyen, Max Wang, Tommy Fan
--- centerimage/centerimage.cc
+++
@@ -1,7 +0,0 @@
-/****
- * Calculates the center of gravity of an image. 
- * Approach (simple for the first iteration):
- * - create a matrix where pixel values <= 127 are marked 0 and values >= 128 are marked 1
- * - calculate the average location of all 1 valued pixels
- * - shift the average location to move it to the center
- ****/
